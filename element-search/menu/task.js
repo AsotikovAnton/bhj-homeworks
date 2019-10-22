8c7b2ec1a8355de05f028c310cd49cba2242040b
Asotikov Anton <asant@yandex.ru>
2019-10-22 21:54:05 +0300

    Еще добавил корректировки ко 2 задаче

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -11,6 +11,10 @@ function removeActive() {
 
 for (let i = 0; i < menuLink.length; i++) {
     menuLink[i].onclick = function() {
+        if (menuLink[i].nextElementSibling.classList.contains("menu_sub") && menuLink[i].nextElementSibling.classList.contains("menu_active")) {
+            removeActive();
+            return false;
+        }
         removeActive();
 
         if (menuLink[i].nextElementSibling.classList.contains("menu_sub")) {
