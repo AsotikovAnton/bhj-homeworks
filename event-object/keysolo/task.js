b712c025fcd36d6042c0752fddbff52be80f51a6
Asotikov Anton <asant@yandex.ru>
2019-10-23 17:24:00 +0300

    Добавил задачу 2

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    window.addEventListener("keydown", (event) => {
+      console.log(event.key.toLocaleLowerCase());
+      if (this.currentSymbol.textContent.toLocaleLowerCase() === event.key.toLocaleLowerCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
